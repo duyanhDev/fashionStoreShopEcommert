b15715cfe667ff9ce4ead08b48328236189efcb4
DuyanhDev <166149554+duyanhDev@users.noreply.github.com>
2025-09-16 08:18:12 +0700

    Deloy part 2 16-9

--- src/components/SizePredictor/SizePredictor.jsx
+++ src/components/SizePredictor/SizePredictor.jsx
@@ -28,7 +28,7 @@ const SizePredictor = ({ open, onClose }) => {
     setResult(null);
 
     try {
-      const res = await fetch("http://127.0.0.1:8080/predict", {
+      const res = await fetch("https://size-prediction.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -60,7 +60,7 @@ const SizePredictor = ({ open, onClose }) => {
 
     try {
       const res = await fetch(
-        `http://127.0.0.1:8080/feedback/${prediction_id}`,
+        `https://size-prediction.onrender.com/feedback/${prediction_id}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
