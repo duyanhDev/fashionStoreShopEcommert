334da45ac189245145c99a38d9353d06e673d125
DuyanhDev <166149554+duyanhDev@users.noreply.github.com>
2025-10-09 15:08:31 +0700

    Deloy FE 09-10 part 4

--- src/untils/axios.js
+++ src/untils/axios.js
@@ -7,7 +7,7 @@ NProgress.configure({
 });
 
 const instance = axios.create({
-  baseURL: "https://fashionstoreshopecommertbe.onrender.com",
+  baseURL: "https://fashionstoreshopecommertbe.onrender.com/",
 });
 
 // const instance = axios.create({
