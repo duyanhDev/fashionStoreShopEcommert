bc1ab2969539d5a42fbfc1097c2db8cec6fc594c
AnhDTD <dangtrinhduyanh+AnhDTD@users.noreply.github.com>
2025-11-10 09:55:11 +0700

    Deloy FE 10//11 part 2

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -832,7 +832,7 @@ const Home = () => {
                     }%)`,
                   }}
                 >
-                  {feedbackImages?.map((testimonial, index) => (
+                  {feedbackImages.map((testimonial, index) => (
                     <div
                       key={testimonial._id}
                       className={`flex-shrink-0 px-3 ${
@@ -865,7 +865,7 @@ const Home = () => {
                               </h4>
                               <p className="text-blue-100 flex items-center text-sm">
                                 <MapPin className="w-3 h-3 mr-1" />
-                                {testimonial.userId.address.city}
+                                {testimonial?.userId?.address?.city || "N/A"}
                               </p>
                             </div>
                           </div>
